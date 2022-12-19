b67963f41080f483fd685397b156759225e8b1c2
DELL <pranalis7111@gmail.com>
2022-12-19 16:32:04 +0530

    HomePage

---
+++ src/main/java/PageLayer/HomePage.java
@@ -0,0 +1,16 @@
+package PageLayer;
+
+import BaseLayer.BaseClass;
+
+public class HomePage extends BaseClass {
+
+	public static void main(String[] args) {
+		System.out.println("Home Page Git pull");
+
+	}
+
+	public static void HomePageTest() {
+		
+	}
+
+}
---
+++ src/test/java/TestLayer/HomePageTest.java
@@ -0,0 +1,17 @@
+package TestLayer;
+
+import BaseLayer.BaseClass;
+import PageLayer.HomePage;
+
+public class HomePageTest extends BaseClass {
+	{
+		HomePageTest homepage = new HomePageTest();
+		
+		HomePage.HomePageTest();
+	}
+
+	
+		
+	}
+
+
